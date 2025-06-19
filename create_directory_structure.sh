8e79b28d73a73632411d83e9226ed30cfee15dc5
felipedds <felipe_dds@yahoo.com.br>
2025-06-19 14:07:15 +0100

    adding the folders

---
+++ create_directory_structure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir data
+mkdir data/raw
+mkdir data/processed
+mkdir src
+mkdir src/data_collection
+mkdir src/data_preprocessing
+mkdir src/feature_engineering
+mkdir src/modeling
+mkdir src/evaluation
+mkdir models
+mkdir notebooks
+mkdir reports
+touch requirements.txt                                              
