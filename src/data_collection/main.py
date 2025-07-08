7847fa8c4c441de2d96c9288f03ae7d2d755c24e
felipe_dds@yahoo.com.br <felipe_dds@yahoo.com.br>
2025-07-08 23:34:04 +0000

    adding files.
    adding files.

--- src/data_collection/main.py
+++ src/data_collection/main.py
@@ -2,12 +2,12 @@ import os
 import requests
 
 # Destination folder
-DEST_FOLDER = "/workspaces/football-data-co-uk/data/raw"
-os.makedirs(DEST_FOLDER, exist_ok=True)
+DESTINATION_FOLDER = "/Workspace/Repos/felipediasd@gmail.com/football-data-co-uk/data/raw/"
+os.makedirs(DESTINATION_FOLDER, exist_ok=True)
 
 # Base URL and seasons
 base_url = "https://www.football-data.co.uk/mmz4281"
-seasons = ["2021", "2122", "2223", "2324", "2425"]  # Add more seasons as needed
+seasons = ["1920", "2021", "2122", "2223", "2324", "2425"]  # Add more seasons as needed
 leagues = ["E0"]  # Add more league codes like 'E1', 'D1' if needed
 
 # Download files
@@ -15,7 +15,7 @@ for season in seasons:
     for league in leagues:
         file_name = f"{league}.csv"
         url = f"{base_url}/{season}/{file_name}"
-        dest_path = os.path.join(DEST_FOLDER, f"{season}_{file_name}")
+        dest_path = os.path.join(DESTINATION_FOLDER, f"{season}_{file_name}")
         
         print(f"Downloading {url}...")
         response = requests.get(url)
